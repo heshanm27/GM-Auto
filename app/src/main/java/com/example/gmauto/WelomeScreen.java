ca3962575d8b4c892d5f5942abf80f1fec8436a3
B.D.H Madhuranga <39455525+heshanm27@users.noreply.github.com>
2022-03-23 21:30:18 +0530

    login screen implimented

--- app/src/main/java/com/example/gmauto/WelomeScreen.java
+++ app/src/main/java/com/example/gmauto/WelomeScreen.java
@@ -20,7 +20,7 @@ public class WelomeScreen extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent dash = new Intent(WelomeScreen.this,Dashbord.class);
+                Intent dash = new Intent(WelomeScreen.this,login.class);
                 startActivity(dash);
             }
         });
