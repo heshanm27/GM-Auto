bed3b5b04a172d64719c9aca000107145bc7c321
B.D.H Madhuranga <39455525+heshanm27@users.noreply.github.com>
2022-03-23 09:20:34 +0530

    splash screen implimented

---
+++ app/src/main/java/com/example/gmauto/Dashbord.java
@@ -0,0 +1,14 @@
+package com.example.gmauto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashbord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashbord);
+    }
+}
--- app/src/main/java/com/example/gmauto/MainActivity.java
+++ app/src/main/java/com/example/gmauto/MainActivity.java
@@ -2,19 +2,51 @@ package com.example.gmauto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private final int SPLASH_SCREEN = 5000;
+    //animation variable
+    Animation topAnimation,bottomAnimation;
+    ImageView image;
+    TextView logo,subtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        //hide status bar
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+//       getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //animations
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //reference hooks
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.logo);
+        subtitle = findViewById(R.id.subtitle);
+
+        //set animation
+        image.setAnimation(topAnimation);
+        logo.setAnimation(bottomAnimation);
+        subtitle.setAnimation(bottomAnimation);
+
+        //change screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Dashbord.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
