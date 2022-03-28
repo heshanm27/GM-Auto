6fafa6b90fddf5f14324f98987edc5758c816f4d
B.D.H Madhuranga <39455525+heshanm27@users.noreply.github.com>
2022-03-28 13:00:31 +0530

    add login,logout and setup useraccesslevel

--- app/src/main/java/com/example/gmauto/Dashbord.java
+++ app/src/main/java/com/example/gmauto/Dashbord.java
@@ -2,13 +2,35 @@ package com.example.gmauto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.gmauto.databinding.ActivityDashbordBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Dashbord extends AppCompatActivity {
+
+    ActivityDashbordBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashbord);
+        binding = ActivityDashbordBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+
+//        String userName = fAuth.getCurrentUser().getDisplayName();
+//        binding.textView.setText(userName);
+        binding.logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),login.class));
+            }
+        });
     }
 }
