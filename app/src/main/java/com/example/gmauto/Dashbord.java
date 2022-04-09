4616add1a12dadaa1f3ffaa96b4b819675e14456
heshanm27 <heshanm27@gmail.com>
2022-04-09 21:22:09 +0530

    add menu drawer and function

--- app/src/main/java/com/example/gmauto/Dashbord.java
+++ app/src/main/java/com/example/gmauto/Dashbord.java
@@ -1,16 +1,22 @@
 package com.example.gmauto;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.gmauto.databinding.ActivityDashbordBinding;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class Dashbord extends AppCompatActivity {
+public class Dashbord extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     ActivityDashbordBinding binding;
 
@@ -20,15 +26,27 @@ public class Dashbord extends AppCompatActivity {
         binding = ActivityDashbordBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        setSupportActionBar(binding.toolbar);
+        binding.navView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, binding.drawerlayout, binding.toolbar, R.string.nav_drawer_open, R.string.nav_darwer_close);
+        binding.drawerlayout.addDrawerListener(toggle);
+        toggle.syncState();
+        binding.navView.setNavigationItemSelectedListener(this);
+    }
 
-//        String userName = fAuth.getCurrentUser().getDisplayName();
-//        binding.textView.setText(userName);
-//        binding.logout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                FirebaseAuth.getInstance().signOut();
-//                startActivity(new Intent(getApplicationContext(),login.class));
-//            }
-//        });
+    //avoid when press back app close
+    @Override
+    public void onBackPressed() {
+        if (binding.drawerlayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerlayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
