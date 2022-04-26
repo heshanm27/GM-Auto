b0166d93edba939d563f42abe4275c35ea978cec
B.D.Heshan Madhuranga IT20611910 <88667057+HeshanMadhuranga@users.noreply.github.com>
2022-04-26 15:59:01 +0530

    dashbord updated

--- app/src/main/java/com/example/gmauto/Dashbord.java
+++ app/src/main/java/com/example/gmauto/Dashbord.java
@@ -23,7 +23,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.gmauto.databinding.ActivityDashbordBinding;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
